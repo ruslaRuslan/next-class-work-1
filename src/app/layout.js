3da6d10af02ee3fb87b44b011e7c17c464fa11d3
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-03 15:36:07 +0400

    header ve footer-i teyin etdik hara kecid etsek ikiside qalacaq

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div>bu navbardi</div>
+        {children}
+        <div>bu footerdir</div>
+      </body>
     </html>
   );
 }
