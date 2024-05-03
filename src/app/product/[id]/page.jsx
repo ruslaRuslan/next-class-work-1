53b0c1b3577386cacd83db8c8cffb8d343980d86
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-03 16:42:36 +0400

    datalarin title ve description-larini ekrana cixartdix

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,8 +1,3 @@
-export default function index({params}){
-    console.log(params.id);
-    return(
-        <>
-        salam product nomre {params.id}
-        </>
-    )
+export default function index({ params }) {
+  return <>salam product nomre {params.id}</>;
 }
