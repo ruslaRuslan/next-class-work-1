bdd8f3530a536c33793d08536c071160d06da2e0
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-03 16:50:04 +0400

    id-lere gore axtaris edirik

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,3 +1,10 @@
-export default function index({ params }) {
-  return <>salam product nomre {params.id}</>;
+import axios from "axios";
+
+const url = "https://dummyjson.com/products";
+
+export default async function index({ params }) {
+  const { data } = await axios.get(url + `/${params.id}`);
+ 
+
+  return <>salam product-in adi  {data.title}</>;
 }
