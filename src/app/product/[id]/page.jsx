69c2b6d9fefec7ac55c9041b97a53d4f22d54dc3
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-05-03 16:51:53 +0400

    is tamam

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const url = "https://dummyjson.com/products";
+const url = "https://dummyjson.com/products/";
 
 export default async function index({ params }) {
-  const { data } = await axios.get(url + `/${params.id}`);
+  const { data } = await axios.get(url + params.id);
  
 
   return <>salam product-in adi  {data.title}</>;
